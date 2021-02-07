6df30fcbc1b2794910163f15de8dc250484e496b
Asier Cardoso Sánchez <asier314@gmail.com>
2021-02-07 12:35:55 +0100

    select the dbs to be used

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from major_system import AvailableDatabases, AvailableEncodeSystems
+import pandas as pd
 app = Flask(__name__)
 
 
@@ -11,21 +12,28 @@ def main():
 @app.route('/encode/', methods=['GET'])
 def encode():
     search = request.args.get("search")
+    dbs = request.args.getlist("db")
+    encoder = request.args.getlist("encoder")
     results = None
     search_method = "encode"
+    available_dbs = [db.name for db in AvailableDatabases]
     if search is not None:
-        database = "spanish"
         encoder = "mine"
 
-        word_database = AvailableDatabases[database.upper()].value
-        words = word_database.get_db()
+        words_dbs = []
+        for db in dbs:
+            word_database = AvailableDatabases[db.upper()].value
+            words_db = word_database.get_db()
+            words_db["source"] = db
+            words_dbs.append(words_db)
 
+        words = pd.concat(words_dbs)
         encoder = AvailableEncodeSystems[encoder.upper()].value
 
         encoded_words = encoder.encode(search, words)
 
         results = encoded_words.word.tolist()
-    return render_template("search.html", search_method=search_method, results=results)
+    return render_template("search.html", available_dbs=available_dbs, search_method=search_method, results=results)
 
 @app.route('/decode/')
 def decode():
