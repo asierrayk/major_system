4ee70804cf7e682657b7d71fd1f6786720c4a1b4
Asier Cardoso Sánchez <asier314@gmail.com>
2021-02-07 01:40:29 +0100

    First working version with get params :)

--- app.py
+++ app.py
@@ -1,10 +1,40 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from major_system import AvailableDatabases, AvailableEncodeSystems
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return "Hello World!"
+def main():
+    word = "main"
+    return render_template("index.html", word=word)
+
+@app.route('/encode/')
+def encode():
+    number = request.args.get("number")
+    results = None
+    if number is not None:
+        database = "spanish"
+        encoder = "mine"
+
+        word_database = AvailableDatabases[database.upper()].value
+        words = word_database.get_db()
+
+        encoder = AvailableEncodeSystems[encoder.upper()].value
+
+        encoded_words = encoder.encode(number, words)
+
+        results = encoded_words.word.tolist()
+    return render_template("encode.html", results=results)
+
+@app.route('/decode/')
+def decode():
+    word = "decode"
+    return render_template("index.html", word=word)
+
+@app.route('/configuration/')
+def configuration():
+    word = "configuration"
+    return render_template("index.html", word=word)
 
 if __name__ == '__main__':
     app.run()
